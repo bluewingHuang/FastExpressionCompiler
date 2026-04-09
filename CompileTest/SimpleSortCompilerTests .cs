c734ef7351a165bb7015e13f9e32c6d576b4b2b0
hyabean <hongyuan.jiang@outlook.com>
2026-04-09 08:58:37 +0800

    test

---
+++ CompileTest/SimpleSortCompilerTests .cs
@@ -0,0 +1,334 @@
+﻿using CompileTest.TestDepency;
+using FastExpressionCompiler;
+using System.Linq.Expressions;
+using System.Reflection;
+using static System.Linq.Expressions.Expression;
+
+namespace CompileTest
+{
+
+    [TestClass]
+    public class SimpleSortCompilerTests
+    {
+        ///// <summary>
+        ///// TestPass: Compile a sorting function for sorting integers in ascending order, and verify that it correctly sorts the data.
+        ///// </summary>
+        //[TestMethod]
+        //[DataRow(true, true)]
+        //[DataRow(true, false)]
+        //[DataRow(false, true)]
+        //[DataRow(false, false)]
+        //public void Compile_ShouldSortIntAscending_Pass(bool UseFastExpressionCompiler, bool useHeapSort)
+        //{
+        //    var compiler = new SortCompiler()
+        //    {
+        //        Orders = new[] { new OrderInfo { Field = "f1", Order = EnumOrderMode.ASC, NilMode = EnumNilMode.LAST } },
+        //        FieldDataTypes = new[] { EnumFieldDataType.INT },
+        //        Capacity = 8
+        //    };
+
+        //    Action<SortData, int, int, int> sortFun = null;
+        //    if (useHeapSort)
+        //        sortFun = compiler.CompileHeap(UseFastExpressionCompiler, true, CompilerFlags.EnableDelegateDebugInfo);
+        //    else
+        //        sortFun = compiler.Compile(UseFastExpressionCompiler, true, CompilerFlags.EnableDelegateDebugInfo);
+        //    var sortData = new SortData
+        //    {
+        //        Columns = new ColumnDataBase[] { CreateIntColumn("Age", 3, 4, 1, 2) },
+        //        RowNumbers = new[] { 0, 1, 2, 3 },
+        //        NilValues = new[,] { { 1, -1 } },
+        //        isAsc = new[] { true },
+        //        Begin = 0,
+        //        End = 3,
+        //        SortFun = sortFun
+        //    };
+
+        //    sortFun(sortData, 0, sortData.RowNumbers.Length - 1, 16);
+
+        //    CollectionAssert.AreEqual(new[] { 2, 3, 0, 1 }, sortData.RowNumbers);
+        //}
+
+        private static ColumnData<int?> CreateIntColumn(string name, params int?[] values)
+        {
+            var column = new ColumnData<int?>(EnumFieldDataType.INT, capacity: 8) { Name = name };
+            for (var i = 0; i < values.Length; i++)
+            {
+                column.Set(0, i, values[i]);
+            }
+
+            return column;
+        }
+
+        ///// <summary>
+        ///// TestPass: Compile a sorting function for sorting integers in ascending order, and verify that it correctly sorts the data.
+        ///// </summary>
+        //[TestMethod]
+        //[DataRow(true, true)]
+        //[DataRow(true, false)]
+        //[DataRow(false, true)]
+        //[DataRow(false, false)]
+        //public void Compile_ShouldSortStringAscending_Pass(bool UseFastExpressionCompiler, bool useHeapSort)
+        //{
+        //    var compiler = new SortCompiler()
+        //    {
+        //        Orders = new[] { new OrderInfo { Field = "f1", Order = EnumOrderMode.ASC, NilMode = EnumNilMode.LAST } },
+        //        FieldDataTypes = new[] { EnumFieldDataType.STRING },
+        //        Capacity = 8
+        //    };
+
+        //    Action<SortData, int, int, int> sortFun = null;
+        //    if (useHeapSort)
+        //        sortFun = compiler.CompileHeap(UseFastExpressionCompiler, true, CompilerFlags.EnableDelegateDebugInfo);
+        //    else
+        //        sortFun = compiler.Compile(UseFastExpressionCompiler, true, CompilerFlags.EnableDelegateDebugInfo);
+        //    var sortData = new SortData
+        //    {
+        //        Columns = new ColumnDataBase[] { CreateStringColumn("UserName", "cc", "dd", "aa", "bb") },
+        //        RowNumbers = new[] { 0, 1, 2, 3 },
+        //        NilValues = new[,] { { 1, -1 } },
+        //        isAsc = new[] { true },
+        //        Begin = 0,
+        //        End = 3,
+        //        SortFun = sortFun,
+        //        StringComparer = new OracleStringComparer()
+        //    };
+
+        //    sortFun(sortData, 0, sortData.RowNumbers.Length - 1, 16);
+
+        //    CollectionAssert.AreEqual(new[] { 2, 3, 0, 1 }, sortData.RowNumbers);
+        //}
+
+        //[TestMethod]
+        //[DataRow(true, true)]
+        //[DataRow(true, false)]
+        //[DataRow(false, true)]
+        //[DataRow(false, false)]
+        //public void Compile_TwoRow2_ShouldSortStringAscending(bool UseFastExpressionCompiler, bool useHeapSort)
+        //{
+        //    var compiler = new SortCompiler()
+        //    {
+        //        Orders = new[] { new OrderInfo { Field = "UserName", Order = EnumOrderMode.ASC, NilMode = EnumNilMode.LAST }, 
+        //            new OrderInfo { Field = "UserName2", Order = EnumOrderMode.ASC, NilMode = EnumNilMode.LAST } },
+        //        FieldDataTypes = new[] { EnumFieldDataType.STRING, EnumFieldDataType.STRING },
+        //        Capacity = 8
+        //    };
+
+        //    Action<SortData, int, int, int> sortFun = null;
+        //    if (useHeapSort)
+        //        sortFun = compiler.CompileHeap(UseFastExpressionCompiler, true, CompilerFlags.EnableDelegateDebugInfo);
+        //    else
+        //        sortFun = compiler.Compile(UseFastExpressionCompiler, true, CompilerFlags.EnableDelegateDebugInfo);
+        //    var sortData = new SortData
+        //    {
+        //        Columns = new ColumnDataBase[] { CreateStringColumn("UserName", "bb", "bb", "aa", "aa"),
+        //            CreateStringColumn("UserName2", "dd", "cc", "dd", "cc") },
+        //        RowNumbers = new[] { 0, 1, 2, 3 },
+        //        //NilValues = new[,] { { 1, -1 }, { 2, -1 } },
+        //        NilValues = new[,] { { 0, -1 }, { 1, -1 } },
+        //        isAsc = new[] { true, true },
+        //        Begin = 0,
+        //        End = 3,
+        //        SortFun = sortFun,
+        //        StringComparer = new OracleStringComparer()
+        //    };
+
+        //    sortFun(sortData, 0, sortData.RowNumbers.Length - 1, 16);
+
+        //    CollectionAssert.AreEqual(new[] { 3, 2, 1, 0 }, sortData.RowNumbers);
+        //}
+
+        //private static ColumnData<string> CreateStringColumn(string name, params string[] values)
+        //{
+        //    var column = new ColumnData<string>(EnumFieldDataType.STRING, capacity: 50) { Name = name };
+        //    for (var i = 0; i < values.Length; i++)
+        //    {
+        //        column.Set(0, i, values[i]);
+        //    }
+
+        //    return column;
+        //}
+
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void CompileComparisonFunction_ShouldReturnExpectedSign_ForDescendingInt(bool UseFastExpressionCompiler)
+        {
+            var compiler = new SortCompiler()
+            {
+                Orders = new[] { new OrderInfo { Field = "f1", Order = EnumOrderMode.DESC, NilMode = EnumNilMode.LAST } },
+                FieldDataTypes = new[] { EnumFieldDataType.INT },
+                Capacity = 8
+            };
+
+            var comparer = compiler.CompileComparisonFunction(UseFastExpressionCompiler);
+            var sortData = new SortData
+            {
+                Columns = new ColumnDataBase[] { CreateIntColumn("age", 3, 1) },
+                RowNumbers = new[] { 0, 1 },
+                NilValues = new[,] { { 1, -1 } },
+                isAsc = new[] { false },
+                StringComparer = new OracleStringComparer(),
+                //ChineseStringComparer = new ChineseStringComparer()
+            };
+
+            Assert.AreEqual(-1, comparer(sortData, 0, 1));
+            Assert.AreEqual(1, comparer(sortData, 1, 0));
+        }
+
+
+        [TestMethod]
+        public void CompileComparisonFunction2_ShouldReturnExpectedSign_ForDescendingInt()
+        {
+            int Capacity = 8;
+            var Orders = new[] { new OrderInfo { Field = "f1", Order = EnumOrderMode.DESC, NilMode = EnumNilMode.LAST } };
+            var FieldDataTypes = new[] { EnumFieldDataType.INT };
+
+            var sortData = Parameter(typeof(SortData), "sortData");
+            var leftIndex = Parameter(typeof(int), "leftIndex");
+            var rightIndex = Parameter(typeof(int), "rightIndex");
+            LabelTarget endMain = Label(typeof(int), "endMain");
+
+            List<Expression> exps = new List<Expression>();
+            List<ParameterExpression> vars = new List<ParameterExpression>();
+            //init(vars, exps);
+
+
+            var left_ListIndex = Expression.Parameter(typeof(int), "left_ListIndex");
+            var left_ArrayIndex = Expression.Parameter(typeof(int), "left_ArrayIndex");
+            var right_ListIndex = Expression.Parameter(typeof(int), "right_ListIndex");
+            var right_ArrayIndex = Expression.Parameter(typeof(int), "right_ArrayIndex");
+            var compareResult = Expression.Parameter(typeof(int), "compareResult");
+            vars.AddRange(new ParameterExpression[] { left_ListIndex, left_ArrayIndex, right_ListIndex, right_ArrayIndex, compareResult });
+
+            var columns = new ParameterExpression[Orders.Length];
+            var leftVars = new ParameterExpression[Orders.Length];
+            var rightVars = new ParameterExpression[Orders.Length];
+            for (int k = 0; k < Orders.Length; ++k)
+            {
+                columns[k] = Expression.Variable(typeof(ColumnData<int?>), $"column_{k}");
+                leftVars[k] = Expression.Parameter(typeof(int?), $"left_{k}");
+                rightVars[k] = Expression.Parameter(typeof(int?), $"right_{k}");
+                vars.Add(leftVars[k]);
+                vars.Add(rightVars[k]);
+                vars.Add(columns[k]);
+                exps.Add(Expression.Assign(columns[k], Expression.Convert(Expression.ArrayAccess(Expression.Field(sortData, "Columns"), Expression.Constant(k)), typeof(ColumnData<int?>))));
+            }
+
+            //exps.AddRange(makeCondition(leftIndex, rightIndex, Expression.Return(endMain, Expression.Constant(1)), Expression.Return(endMain, Expression.Constant(-1))));
+
+            Expression breakExp = Expression.Return(endMain, Expression.Constant(1));
+            Expression continueExp = Expression.Return(endMain, Expression.Constant(-1));
+            exps.Add(Expression.Assign(left_ListIndex, Expression.Divide(leftIndex, Expression.Constant(Capacity))));
+            exps.Add(Expression.Assign(left_ArrayIndex, Expression.Modulo(leftIndex, Expression.Constant(Capacity))));
+
+            exps.Add(Expression.Assign(right_ListIndex, Expression.Divide(rightIndex, Expression.Constant(Capacity))));
+            exps.Add(Expression.Assign(right_ArrayIndex, Expression.Modulo(rightIndex, Expression.Constant(Capacity))));
+
+
+            for (int k = 0; k < Orders.Length; ++k)
+            {
+                var dataArrayList = Expression.PropertyOrField(Expression.PropertyOrField(columns[k], "Datas"), "DataArrayList");
+
+                exps.Add(Expression.Assign(leftVars[k], Expression.ArrayAccess(Expression.Property(dataArrayList, "Item", left_ListIndex), left_ArrayIndex)));
+                exps.Add(Expression.Assign(rightVars[k], Expression.ArrayAccess(Expression.Property(dataArrayList, "Item", right_ListIndex), right_ArrayIndex)));
+
+                var leftNil = Expression.IfThen(Expression.NotEqual(rightVars[k], Expression.Constant(null)), Expression.IfThenElse(Expression.Equal(Expression.ArrayAccess(Expression.Field(sortData, "NilValues"), Expression.Constant(k), Expression.Constant(0)), Expression.Constant(-1)), continueExp, breakExp));
+                var rightNil = Expression.IfThen(Expression.NotEqual(leftVars[k], Expression.Constant(null)), Expression.IfThenElse(Expression.Equal(Expression.ArrayAccess(Expression.Field(sortData, "NilValues"), Expression.Constant(k), Expression.Constant(1)), Expression.Constant(-1)), continueExp, breakExp));
+
+                var isAsc = Expression.ArrayAccess(Expression.Field(sortData, "isAsc"), Expression.Constant(k));
+                List<Expression> allNotNil = new List<Expression>();
+                MethodInfo compareMethod = typeof(OracleStringComparer).GetMethod("Compare", new Type[] { typeof(string), typeof(string) });
+                Expression stringCompare = Expression.Call(Expression.Field(sortData, "StringComparer"), compareMethod, leftVars[k], rightVars[k]);
+
+                if (FieldDataTypes[k] == EnumFieldDataType.STRING)
+                    allNotNil.Add(Expression.Assign(compareResult, stringCompare));
+                    //if (Orders[k].Order == EnumOrderMode.ASCCH || Orders[k].Order == EnumOrderMode.DESCCH)
+                    //    allNotNil.Add(Expression.Assign(compareResult, chineseStringCompare(leftVars[k], rightVars[k])));
+                    //else
+                    //    allNotNil.Add(Expression.Assign(compareResult, stringCompare(leftVars[k], rightVars[k])));
+                else
+                {
+                    var method = typeof(int?).GetMethod("CompareTo", new[] { typeof(int?) });
+                    allNotNil.Add(Expression.Assign(compareResult, Expression.Call(Expression.PropertyOrField(leftVars[k], "Value"), method, Expression.PropertyOrField(rightVars[k], "Value"))));
+                }
+
+                allNotNil.Add(Expression.IfThen(Expression.Not(isAsc), Expression.Assign(compareResult, Expression.Negate(compareResult))));
+                allNotNil.Add(Expression.IfThen(Expression.Equal(compareResult, Expression.Constant(-1)), continueExp));
+
+                if (k < Orders.Length - 1)
+                    allNotNil.Add(Expression.IfThen(Expression.Equal(compareResult, Expression.Constant(1)), breakExp));
+
+
+                var exp = Expression.IfThenElse(Expression.Equal(leftVars[k], Expression.Constant(null)), leftNil, Expression.IfThenElse(Expression.Equal(rightVars[k], Expression.Constant(null)), rightNil, Expression.Block(allNotNil.ToArray())));
+                exps.Add(exp);
+            }
+            exps.Add(breakExp);
+
+            exps.Add(Expression.Label(endMain, Expression.Constant(-1)));
+            BlockExpression block = Expression.Block(
+                vars.ToArray(), exps
+            );
+            var expr = Lambda<Func<SortData, int, int, int>>(block, sortData, leftIndex, rightIndex);
+            expr.PrintCSharp();
+
+            var data1 = new SortData
+            {
+                Columns = new ColumnDataBase[] { CreateIntColumn("age", 3, 1) },
+                RowNumbers = new[] { 0, 1 },
+                NilValues = new[,] { { 1, -1 } },
+                isAsc = new[] { false },
+                StringComparer = new OracleStringComparer(),
+                //ChineseStringComparer = new ChineseStringComparer()
+            };
+            var data2 = new SortData
+            {
+                Columns = new ColumnDataBase[] { CreateIntColumn("age", 3, 1) },
+                RowNumbers = new[] { 0, 1 },
+                NilValues = new[,] { { 1, -1 } },
+                isAsc = new[] { false },
+                StringComparer = new OracleStringComparer(),
+                //ChineseStringComparer = new ChineseStringComparer()
+            };
+
+
+            var fs = expr.CompileSys();
+            fs.PrintIL();
+            var result1 = fs(data1, 0, 1);
+
+            var ff = expr.CompileFast(ifFastFailedReturnNull: true);
+            //t.IsNotNull(ff);
+            ff.PrintIL();
+            var result2 = ff(data2, 0, 1);
+
+            //t.AreEqual(data1, data2);
+            Assert.AreEqual(result1, result2);
+
+
+            //if (UseFastExpressionCompiler)
+            //{
+            //    var re = Lambda<Func<SortData, int, int, int>>(block, sortData, leftIndex, rightIndex).CompileFast();
+            //    return re;
+            //}
+            //else
+            //{
+            //    var re = Expression.Lambda<Func<SortData, int, int, int>>(block, sortData, leftIndex, rightIndex).CompileSys();
+            //    return re;
+            //}
+
+            //var comparer = compiler.CompileComparisonFunction(UseFastExpressionCompiler);
+            //var sortData = new SortData
+            //{
+            //    Columns = new ColumnDataBase[] { CreateIntColumn("age", 3, 1) },
+            //    RowNumbers = new[] { 0, 1 },
+            //    NilValues = new[,] { { 1, -1 } },
+            //    isAsc = new[] { false },
+            //    StringComparer = new OracleStringComparer(),
+            //    //ChineseStringComparer = new ChineseStringComparer()
+            //};
+
+            //Assert.AreEqual(-1, comparer(sortData, 0, 1));
+            //Assert.AreEqual(1, comparer(sortData, 1, 0));
+        }
+    }
+}
--- test/FastExpressionCompiler.TestsRunner/Program.cs
+++ test/FastExpressionCompiler.TestsRunner/Program.cs
@@ -52,27 +52,28 @@ namespace FastExpressionCompiler.UnitTests
             var st = new TestRun(TestFlags.RethrowException);
 
             st.Run(new Issue499_InvalidProgramException_for_Sorting_and_comparison_function());
-            st.Run(new Issue498_InvalidProgramException_when_using_loop());
-            st.Run(new Issue495_Incomplete_pattern_detection_for_NotSupported_1007_Return_goto_from_TryCatch_with_Assign_generates_invalid_IL());
-            st.Run(new Issue480_CLR_detected_an_invalid_program_exception());
-#if NET8_0_OR_GREATER
-            st.Run(new Issue487_Fix_ToCSharpString_output_for_boolean_equality_expressions());
-            st.Run(new Issue475_Reuse_DynamicMethod_if_possible());
-#endif
+//            st.Run(new Issue498_InvalidProgramException_when_using_loop());
+//            st.Run(new Issue495_Incomplete_pattern_detection_for_NotSupported_1007_Return_goto_from_TryCatch_with_Assign_generates_invalid_IL());
+//            st.Run(new Issue480_CLR_detected_an_invalid_program_exception());
+//#if NET8_0_OR_GREATER
+//            st.Run(new Issue487_Fix_ToCSharpString_output_for_boolean_equality_expressions());
+//            st.Run(new Issue475_Reuse_DynamicMethod_if_possible());
+//#endif
 
             var lt = new LightExpression.TestRun(LightExpression.TestFlags.RethrowException);
 
-#if NET8_0_OR_GREATER
-            lt.Run(new LightExpression.IssueTests.Issue490_Regression_in_compiling_lambdas_with_ref_struct_parameters());
-#endif
+//#if NET8_0_OR_GREATER
+//            lt.Run(new LightExpression.IssueTests.Issue490_Regression_in_compiling_lambdas_with_ref_struct_parameters());
+//#endif
 
-            lt.Run(new LightExpression.IssueTests.Issue398_Optimize_Switch_with_OpCodes_Switch());
-            lt.Run(new LightExpression.IssueTests.Issue468_Optimize_the_delegate_access_to_the_Closure_object_for_the_modern_NET());
-            lt.Run(new LightExpression.IssueTests.Issue472_TryInterpret_and_Reduce_primitive_arithmetic_and_logical_expressions_during_the_compilation());
-            lt.Run(new LightExpression.IssueTests.Issue473_InvalidProgramException_when_using_Expression_Condition_with_converted_decimal_expression());
-            lt.Run(new LightExpression.IssueTests.Issue476_System_ExecutionEngineException_with_nullables_on_repeated_calls_to_ConcurrentDictionary());
+//            lt.Run(new LightExpression.IssueTests.Issue398_Optimize_Switch_with_OpCodes_Switch());
+//            lt.Run(new LightExpression.IssueTests.Issue468_Optimize_the_delegate_access_to_the_Closure_object_for_the_modern_NET());
+//            lt.Run(new LightExpression.IssueTests.Issue472_TryInterpret_and_Reduce_primitive_arithmetic_and_logical_expressions_during_the_compilation());
+//            lt.Run(new LightExpression.IssueTests.Issue473_InvalidProgramException_when_using_Expression_Condition_with_converted_decimal_expression());
+//            lt.Run(new LightExpression.IssueTests.Issue476_System_ExecutionEngineException_with_nullables_on_repeated_calls_to_ConcurrentDictionary());
             lt.Run(new LightExpression.IssueTests.Issue499_InvalidProgramException_for_Sorting_and_comparison_function());
 
+            return;
             RunAllTests();
         }
 
